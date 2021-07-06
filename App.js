71c02649daed28d32f96e47f0ec269941dd73be9
Luis Guilherme <luis-guilherme.pereira@unesp.br>
2021-07-06 07:02:07 -0300

    navigator with problem

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import AppNavigator from './app.navigator';
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default class App extends React.Component<{}> {
+    render(){
+      return (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+            <AppNavigator />
+          <Text>Maria Clara Linda</Text>
+          <Text>plic ploc!</Text>
+        </View>
+      );
+    }
 }
